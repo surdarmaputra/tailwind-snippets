fdc7c57811a0c61140cb2871e91064a0864818c1
Surya Darma Putra <surya.igede@gmail.com>
2022-08-28 15:19:51 +0800

    fix: component directory alias in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -27,6 +27,10 @@ module.exports = {
         jsx: 'react',
       }),
     );
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      components: path.resolve(__dirname, '../components'),
+    };
 
     return config;
   },
