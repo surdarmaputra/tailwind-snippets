0b40adf07b167bba9a47c8e3410d79fb7d90077a
Surya Darma Putra <surya.igede@gmail.com>
2022-08-28 15:27:18 +0800

    feat: snippets path

---
+++ pages/snippets/index.tsx
@@ -0,0 +1,24 @@
+import SnippetsExplorerLayout from 'components/templates/SnippetsExplorerLayout';
+import { InferGetStaticPropsType } from 'next';
+import getSnippets from 'utils/getStaticProps/getSnippets';
+
+import ListSearchIcon from '~icons/tabler/list-search';
+
+export async function getStaticProps() {
+  return getSnippets();
+}
+
+export default function Snippets({
+  snippets,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <SnippetsExplorerLayout
+      className="flex  flex-col items-center py-44 text-center"
+      snippets={snippets}
+    >
+      <ListSearchIcon className="mb-12 h-32 w-32 text-dark-200" />
+      <h2>No item selected</h2>
+      <p>Please select any snippet from the navigation on the left.</p>
+    </SnippetsExplorerLayout>
+  );
+}
