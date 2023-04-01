8ac8b6e6e1ddaf9088ba20af3be119e369ec03b8
surdarmaputra <surya.igede@gmail.com>
2023-04-01 21:42:28 +0700

    chore: install gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,24 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID}');
+          `}
+        </Script>
+      </Head>
       <link href="https://fonts.googleapis.com" rel="preconnect" />
       <link crossOrigin="" href="https://fonts.gstatic.com" rel="preconnect" />
       <link
